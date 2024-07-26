19513f91455deab2d392d5c51bb69b631e0aa3ea
Bernhard <72475338+beloureiro@users.noreply.github.com>
2024-07-26 12:12:08 +0100

    Delete src directory

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import BusinessManagementPage from './BusinessManagementPage';
-
-ReactDOM.render(<BusinessManagementPage />, document.getElementById('root'));
