81f6b73cf159748ede084a64643dc657ca11ac7f
Bernhard <72475338+beloureiro@users.noreply.github.com>
2024-07-26 11:45:31 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import BusinessManagementPage from './BusinessManagementPage';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BusinessManagementPage />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<BusinessManagementPage />, document.getElementById('root'));
